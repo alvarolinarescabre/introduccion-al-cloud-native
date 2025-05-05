6da40c3adf574cec0e4bfde3222407133c8d984a
Alvaro 'Chamo' Linares Cabre <alinares@paradigmadigital.com>
2025-05-06 01:41:30 +0200

    Refactor to function and add concurrency

--- chamo_api/main.go
+++ chamo_api/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
+	"net/http"
 	"strings"
-	"github.com/gocolly/colly"
+	"sync"
+	"time"
+
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humachi"
 	"github.com/danielgtaylor/huma/v2/humacli"
 	"github.com/go-chi/chi/v5"
-	"net/http"
+	"github.com/gocolly/colly"
 
 	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
 )
@@ -29,35 +31,59 @@ type HealthOutput struct {
 
 // Link response
 type Link struct {
-	Id      int    `json:"id" doc:"Id of the resource"`
-	Url     string `json:"url,omitempty" doc:"Url to search"`
-	Links	int	   `json:"links" doc:"Number of the links finds"`
-	Time    string `json:"time" doc:"Time take to search"`
+	Id    int    `json:"id" doc:"Id of the resource"`
+	Url   string `json:"url,omitempty" doc:"Url to search"`
+	Links int    `json:"links" doc:"Number of the links finds"`
+	Time  string `json:"time" doc:"Time take to search"`
 }
 
 // Links response
 type LinksOutput struct {
 	Body struct {
-		Links []Link `json:"links" doc:"Links to search"`      
+		Links []Link `json:"links" doc:"Links to search"`
 	}
 }
 
-func addRoutes(api huma.API) {
+// 10 Websites and search https and http
+var urls = []string{
+	"https://www.holachamo.com",
+	"https://www.paradigmadigital.com",
+	"https://www.realpython.com",
+	"https://www.lapatilla.com",
+	"https://www.facebook.com",
+	"https://www.gitlab.com",
+	"https://www.youtube.com",
+	"https://www.mozilla.org",
+	"https://www.github.com",
+	"https://www.google.com",
+}
 
-	// 10 Websites and search https and http
-	urls := []string{
-		"https://www.holachamo.com",
-		"https://www.paradigmadigital.com",
-		"https://www.realpython.com",
-		"https://www.lapatilla.com",
-		"https://www.facebook.com",
-		"https://www.gitlab.com",
-		"https://www.youtube.com",
-		"https://www.mozilla.org",
-		"https://www.github.com",
-		"https://www.google.com",
-	}
+func webScrapingCounter(url string) int {
 
+	count := 0
+
+	// Create a new collector
+	c := colly.NewCollector(
+		colly.MaxDepth(1),
+	)
+
+	// Set a callback for when a visited HTML element is found
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if link == "" {
+			return
+		} else {
+			count += strings.Count(link, "https://")
+			count += strings.Count(link, "http://")
+		}
+	})
+
+	c.Visit(url)
+
+	return count
+}
+
+func getHealthCheck(api huma.API) {
 	// Add GET / for health checks
 	huma.Register(api, huma.Operation{
 		OperationID:   "get-health",
@@ -70,6 +96,62 @@ func addRoutes(api huma.API) {
 		resp.Body.Status = "ok"
 		return resp, nil
 	})
+
+}
+
+func getLink(api huma.API) {
+
+	link := make([]Link, 0)
+
+	// Register GET /v1/link/{id}
+	// This endpoint will search for link in only one of 10 websites
+	huma.Register(api, huma.Operation{
+		OperationID: "get-link",
+		Method:      http.MethodGet,
+		Path:        "/v1/link/{id}",
+		Summary:     "Get link.",
+		Description: "Get link to 'https' and 'http' search for one of 10 websites",
+		Tags:        []string{"Links"},
+	}, func(ctx context.Context, input *struct {
+		Id int `path:"id" maxLength:"2" example:"0" doc:"Id of website from array"`
+	}) (*LinksOutput, error) {
+		resp := &LinksOutput{}
+
+		start := time.Now()
+
+		fmt.Println("Starting to search link...")
+
+		index := 0
+
+		id := input.Id
+		// Check if the id is between 0 and 9
+		if id < 0 || id > 9 {
+			return nil, fmt.Errorf("id must be between 0 and 9")
+		}
+		// Get the url from the urls array
+		url := urls[id]
+
+		count := webScrapingCounter(url)
+
+		timeElapsed := time.Since(start)
+
+		link = append(link, Link{
+			Id:    index,
+			Url:   urls[id],
+			Links: count,
+			Time:  timeElapsed.String(),
+		})
+
+		resp.Body.Links = link
+		fmt.Printf("id: %d | url: %s | link: %d | time: %s\n", index, urls[id], count, timeElapsed.String())
+
+		fmt.Println("Finished searching link.")
+
+		return resp, nil
+	})
+}
+
+func getLinks(api huma.API) {
 
 	links := make([]Link, 0)
 
@@ -89,116 +171,28 @@ func addRoutes(api huma.API) {
 
 		fmt.Println("Starting to search links...")
 
-		// Create a new collector
-		c := colly.NewCollector(
-			colly.MaxDepth(10),
-			colly.Async(true),
-		)
-		
-		
-		c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 10})
-
 		// Set a callback for when a visited HTML element is found
 		for index, url := range urls {
-			count := 0
-			
-			c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-				link := e.Attr("href")
-				if link == "" {
-					return
-				} else {
-					count += strings.Count(link, "https://")
-					count += strings.Count(link, "http://")
-				}
-			})
-	
-	
-			c.Visit(url)
-			c.Wait()
-			
+
+			wg := sync.WaitGroup{}
+			wg.Add(1)
+			count := webScrapingCounter(url)
+			wg.Done()
+			wg.Wait()
+
 			timeElapsed := time.Since(start)
 
 			links = append(links, Link{
-				Id:      index,
-				Url:     url,
-				Links:	 count,
-				Time:    timeElapsed.String(),
+				Id:    index,
+				Url:   url,
+				Links: count,
+				Time:  timeElapsed.String(),
 			})
 			resp.Body.Links = links
 			fmt.Printf("id: %d | url: %s | links: %d | time: %s\n", index, url, count, timeElapsed.String())
 		}
 
 		fmt.Println("Finished searching links.")
-
-		return resp, nil
-	})
-
-
-	// Register GET /v1/link/{id}
-	// This endpoint will search for link in only one of 10 websites
-	huma.Register(api, huma.Operation{
-		OperationID: "get-link",
-		Method:      http.MethodGet,
-		Path:        "/v1/link/{id}",
-		Summary:     "Get link.",
-		Description: "Get link to 'https' and 'http' search for one of 10 websites",
-		Tags:        []string{"Links"},
-	}, func(ctx context.Context, input *struct{
-		Id int `path:"id" maxLength:"2" example:"0" doc:"Id of website from array"`
-	}) (*LinksOutput, error) {
-		resp := &LinksOutput{}
-
-		start := time.Now()
-
-		fmt.Println("Starting to search link...")
-
-		count := 0
-		index := 0
-
-		id := input.Id
-		// Check if the id is between 0 and 9
-		if id < 0 || id > 9 {
-			return nil, fmt.Errorf("id must be between 0 and 9")
-		}
-		// Get the url from the urls array
-		url := urls[id]
-
-		// Create a new collector
-		c := colly.NewCollector(
-			colly.MaxDepth(1),
-			colly.Async(true),
-		)
-		
-		c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 1})
-
-		// Set a callback for when a visited HTML element is found
-		c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-			link := e.Attr("href")
-			if link == "" {
-				return
-			} else {
-				count += strings.Count(link, "https://")
-				count += strings.Count(link, "http://")
-			}
-		})
-	
-		c.Visit(url)
-		c.Wait()
-		
-		timeElapsed := time.Since(start)
-
-		links = append(links, Link{
-			Id:      index,
-			Url:     urls[id],
-			Links:	 count,
-			Time:    timeElapsed.String(),
-		})
-
-		
-		resp.Body.Links = links
-		fmt.Printf("id: %d | url: %s | link: %d | time: %s\n", index, urls[id], count, timeElapsed.String())
-
-		fmt.Println("Finished searching link.")
 
 		return resp, nil
 	})
@@ -211,7 +205,10 @@ func main() {
 		router := chi.NewMux()
 		api := humachi.New(router, huma.DefaultConfig("Get links to 'https' and 'http' from 10 sites.", "1.0.0"))
 
-		addRoutes(api)
+		// Call functions
+		getHealthCheck(api)
+		getLink(api)
+		go getLinks(api)
 
 		// Tell the CLI how to start your server.
 		hooks.OnStart(func() {
@@ -219,7 +216,6 @@ func main() {
 			http.ListenAndServe(fmt.Sprintf(":%d", options.Port), router)
 		})
 	})
-
 
 	// Start the server!
 	cli.Run()
