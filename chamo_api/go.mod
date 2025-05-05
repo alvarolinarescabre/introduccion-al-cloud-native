7bdd2baa9ed6db757dd9d49d1b80eb1a4b4e4832
Alvaro 'Chamo' Linares Cabre <alinares@paradigmadigital.com>
2025-05-05 11:04:51 +0200

    Add API in Go

---
+++ chamo_api/go.mod
@@ -0,0 +1,30 @@
+module github.com/alvarolinarescabre/chamo-api
+
+go 1.24.2
+
+require (
+	github.com/danielgtaylor/huma/v2 v2.32.0
+	github.com/go-chi/chi/v5 v5.1.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.3 // indirect
+	github.com/anaskhan96/soup v1.2.5 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/antchfx/htmlquery v1.3.4 // indirect
+	github.com/antchfx/xmlquery v1.4.4 // indirect
+	github.com/antchfx/xpath v1.3.3 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+)
---
+++ chamo_api/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+	"strings"
+	"github.com/gocolly/colly"
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humachi"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+
+	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
+)
+
+// Health represents the response of the "get health" operation.
+type HealthOutput struct {
+	Body struct {
+		Status string `json:"status"`
+	}
+}
+
+// Link response
+type Link struct {
+	Id      int    `json:"id" doc:"Id of the resource"`
+	Url     string `json:"url,omitempty" doc:"Url to search"`
+	Total	int	   `json:"total" doc:"Number of the links finds"`
+	Time    string `json:"time" doc:"Time take to search"`
+}
+
+// Links response
+type LinksOutput struct {
+	Body struct {
+		Links []Link `json:"links" doc:"Links to search"`      
+	}
+}
+
+func main() {
+	// 10 Websites and search https and http
+	urls := []string{
+		"https://www.holachamo.com",
+		"https://www.paradigmadigital.com",
+		"https://www.realpython.com",
+		"https://www.lapatilla.com",
+		"https://www.facebook.com",
+		"https://www.gitlab.com",
+		"https://www.youtube.com",
+		"https://www.mozilla.org",
+		"https://www.github.com",
+		"https://www.google.com",
+	}
+
+	// Create a new router & API
+	router := chi.NewMux()
+	api := humachi.New(router, huma.DefaultConfig("Get links to 'https' and 'http' from 10 sites.", "1.0.0"))
+
+	// Add GET / for health checks
+	huma.Register(api, huma.Operation{
+		OperationID:   "get-health",
+		Summary:       "Get health",
+		Method:        http.MethodGet,
+		Path:          "/",
+		DefaultStatus: http.StatusOK,
+	}, func(ctx context.Context, i *struct{}) (*HealthOutput, error) {
+		resp := &HealthOutput{}
+		resp.Body.Status = "ok"
+		return resp, nil
+	})
+
+	links := make([]Link, 0)
+
+	// Register GET /v1/tags
+	huma.Register(api, huma.Operation{
+		OperationID: "get-links",
+		Method:      http.MethodGet,
+		Path:        "/v1/links",
+		Summary:     "Get links to 'https' and 'http' from 10 sites.",
+		Description: "Get links to 'https' and 'http' from 10 sites.",
+		Tags:        []string{"Links"},
+	}, func(ctx context.Context, input *struct{}) (*LinksOutput, error) {
+		resp := &LinksOutput{}
+
+		start := time.Now()
+
+		for index, url := range urls {
+			count := 0
+			tag := "href"
+	
+			c := colly.NewCollector()
+	
+			c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+				link := e.Attr(tag)
+				if link == "" {
+					return
+				} else {
+					count += strings.Count(link, "https://")
+					count += strings.Count(link, "http://")
+				}
+			})
+	
+	
+			c.Visit(url)
+			c.Wait()
+			
+			timeElapsed := time.Since(start)
+
+			links = append(links, Link{
+				Id:      index,
+				Url:     url,
+				Total:	 count,
+				Time:    timeElapsed.String(),
+			})
+			resp.Body.Links = links
+			fmt.Printf("url: %s | tags: %d\n", url, count)
+		}
+
+		return resp, nil
+	})
+
+	// Start the server!
+	http.ListenAndServe("0.0.0.0:8888", router)
+}
